d7acf1bd57f40631e97c584ccadca6bdd7582680
Asnly1 <asnly1013@gmail.com>
2025-10-28 15:27:51 +0800

    version3 of bpe_tokenizer

--- cs336_basics/BPE_tokenizer.py
+++ cs336_basics/BPE_tokenizer.py
@@ -24,18 +24,33 @@ def train_bpe(
         
         word_count: dict[tuple[int, ...], int] = dict()
         
+        special_token_bytes = [special_token.encode("utf-8") for special_token in special_tokens]
+        special_tokens_pattern = b"|".join(re.escape(special_token_byte) for special_token_byte in special_token_bytes )
+        
         # Pre-tokenization
         for start, end in zip(boundaries[:-1], boundaries[1:]): # todo: parallelize
             f.seek(start)
-            chunk = f.read(end - start).decode("utf-8", errors="ignore")
-            
-            for word in re.finditer(PAT, chunk):
-                word_bytes: tuple[int, ...] = tuple(word.group(0).encode("utf-8"))
-                word_count[word_bytes] = word_count.get(word_bytes, 0) + 1
-                
-        # Get initial bytes_pair count
+            chunk = f.read(end - start)
+            chunk_list: list[bytes] = re.split(special_tokens_pattern, chunk)
+            for chunk in chunk_list:
+                chunk = chunk.decode("utf-8")
+                for word in re.finditer(PAT, chunk):
+                    word_bytes: tuple[int, ...] = tuple(word.group(0).encode("utf-8"))
+                    word_count[word_bytes] = word_count.get(word_bytes, 0) + 1
         
-        bytes_pair_count: dict[tuple[int, int], int] = get_bytes_pair_count(word_count)
+        # Map bytes_pair to its word  
+        bytes_pair_to_word: dict[tuple[int, int], set[tuple[int, ...]]] = dict()
+        # Get initial bytes_pair count
+        bytes_pair_count: dict[tuple[int, int], int] = dict()
+        
+        for word, count in word_count.items():
+            pair_list = get_pairs_from_word(word)
+            for pair in pair_list:
+                bytes_pair_count[pair] = bytes_pair_count.get(pair, 0) + count
+                
+                if pair not in bytes_pair_to_word.keys():
+                    bytes_pair_to_word[pair] = set()
+                bytes_pair_to_word[pair].add(word)
             
         # Train Tokenizer
         special_tokens_length = len(special_tokens)
@@ -43,50 +58,61 @@ def train_bpe(
         
         for i in range(num_merge):
             most_frequent_pair: tuple[int, int] = get_most_frequent_pair(bytes_pair_count, vocab)
+            if most_frequent_pair is None:
+                break
             
-            token1 = vocab.get(most_frequent_pair[0])
-            token2 = vocab.get(most_frequent_pair[1])
-            new_token_bytes = token1 + token2
+            token1_bytes = vocab.get(most_frequent_pair[0])
+            token2_bytes = vocab.get(most_frequent_pair[1])
+            new_token_bytes = token1_bytes + token2_bytes
             new_token_id = i+256+special_tokens_length
             
             vocab[new_token_id] = new_token_bytes
             vocab_bytes_to_int[new_token_bytes] = new_token_id
-            merges.append((token1, token2))
+            merges.append((token1_bytes, token2_bytes))
             
-            word_count_new: dict[tuple[int, ...], int] = dict()
+            word_need_to_recount: list[tuple[int, ...]] = bytes_pair_to_word.get(most_frequent_pair)
             
-            for word, count in word_count.items():
-                new_word = replace_word_with_new_token(word, most_frequent_pair[0], most_frequent_pair[1], new_token_id)
+            # Delete old pairs
+            for word in word_need_to_recount:
+                count = word_count.get(word)
+                del word_count[word]
                 
-                word_count_new[new_word] = word_count_new.get(new_word, 0) + count
+                pair_list = get_pairs_from_word(word)
+                for pair in pair_list:
+                    bytes_pair_to_word[pair].remove(word)
+                    bytes_pair_count[pair] = bytes_pair_count.get(pair, 0) - count
+                    if bytes_pair_count.get(pair) == 0:
+                        del bytes_pair_count[pair]
             
-            word_count = word_count_new
-            bytes_pair_count_new: dict[tuple[bytes, bytes], int] = get_bytes_pair_count(word_count)
-            bytes_pair_count = bytes_pair_count_new
+            # Add new pairs
+            for word in word_need_to_recount:
+                word = replace_word_with_new_token(word, most_frequent_pair[0], most_frequent_pair[1], new_token_id)
+                word_count[word] = count
+                
+                pair_list = get_pairs_from_word(word)
+                for pair in pair_list:
+                    bytes_pair_count[pair] = bytes_pair_count.get(pair, 0) + count
+                    
+                    if pair not in bytes_pair_to_word.keys():
+                        bytes_pair_to_word[pair] = set()
+                    bytes_pair_to_word[pair].add(word)
 
     return vocab, merges
-
-def get_bytes_pair_count(word_count: dict[tuple[int, ...], int]) -> dict[tuple[int, int], int]:
-    bytes_pair_count: dict[tuple[int, int], int] = dict()
-    
-    for word_bytes, count in word_count.items():
-        pair_list = get_pairs_from_word(word_bytes)
-        for pair in pair_list:
-            bytes_pair_count[pair] = bytes_pair_count.get(pair, 0) + count
-            
-    return bytes_pair_count
 
 def get_pairs_from_word(word: tuple[int, ...]) -> list[tuple[int, int]]:
     return [(byte1, byte2) for byte1, byte2 in zip(word, word[1:])]
 
 def get_most_frequent_pair(bytes_pair_count: dict[tuple[int, int], int], vocab: dict[int, bytes]) -> tuple[int, int]:
+    if not bytes_pair_count:
+        return None
+    
     max_count = max(bytes_pair_count.values())
     frquent_pairs = [bytes_pair for bytes_pair, count in bytes_pair_count.items() if count == max_count]
     
-    # def helper(bytes_pair: tuple[int, int]) -> tuple[str, str]:
-    #     return str(vocab.get(bytes_pair[0]) + vocab.get(bytes_pair[1]))
+    def helper(bytes_pair: tuple[int, int]) -> tuple[bytes, bytes]:
+        return vocab.get(bytes_pair[0]), vocab.get(bytes_pair[1])
     
-    return max(frquent_pairs)
+    return max(frquent_pairs, key=helper)
 
 def replace_word_with_new_token(word: tuple[int, ...], 
                                 old_token_1:int, 
